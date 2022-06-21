a7d83b224329f1dac6969637e42b2b0285c5121e
yoshida <hamayarawa1239@gmail.com>
2022-06-21 23:58:03 +0900

    test

--- static/js/study.js
+++ static/js/study.js
@@ -17,3 +17,15 @@ $("#incorrect-btn").on('click', function(){
 })
 
 
+let isUrl = $('#correct-answer').text()
+AutoLink(isUrl);
+function AutoLink(str) {
+    var regexp_url = /((h?)(ttps?:\/\/[a-zA-Z0-9.\-_@:/~?%&;=+#',()*!]+))/g; // ']))/;
+    var regexp_makeLink = function(all, url, h, href) {
+        return '<a href="h' + href + '">' + url + '</a>';
+    }
+ 
+    return str.replace(regexp_url, regexp_makeLink);
+}
+
+
