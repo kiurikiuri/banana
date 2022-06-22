46c8dfe8ac69639d86133b60ef39767a64055b90
yoshida <hamayarawa1239@gmail.com>
2022-06-22 22:49:23 +0900

    test

---
+++ static/js/js_autolink.js
@@ -0,0 +1,5 @@
+$(function(){
+    $('.js-autolink').each(function(){
+        $(this).html($(this).html().replace(/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig,"<a href='$1'>$1</a>"));
+    });
+});
