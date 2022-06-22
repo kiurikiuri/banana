178408feac5088b9f1d2868539c836d2b8e983bf
yoshida <hamayarawa1239@gmail.com>
2022-06-22 22:49:00 +0900

    test

--- static/js/study.js
+++ static/js/study.js
@@ -17,8 +17,8 @@ $("#incorrect-btn").on('click', function(){
 })
 
 
-$(function(){
-    $('.js-autolink').each(function(){
-        $(this).html($(this).html().replace(/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig,"<a href='$1'>$1</a>"));
-    });
-});
+// $(function(){
+//     $('.js-autolink').each(function(){
+//         $(this).html($(this).html().replace(/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig,"<a href='$1'>$1</a>"));
+//     });
+// });
